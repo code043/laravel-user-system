02b2df6794ca5aaba54aa9b01ca17ed4bcfd8b75
code043 <procoder043@gmail.com>
2024-11-15 19:21:54 -0300

    Init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User System</title>
+    @vite("./resources/css/app.css")
+</head>
+<body>
+    <main class="container flex justify-center">
+        <h1 class="text-red-400 text-4xl">Users</h1>
+    </main>
+</body>
+</html>
