7151d91b294ca507b96f783bb9255fc90694f5fe
code043 <procoder043@gmail.com>
2024-11-17 14:46:13 -0300

    About navbar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,9 +7,23 @@
   <title>About</title>
 </head>
 <body>
+  <header class="flex justify-between px-3 py-4 bg-black">
+    <div class="text-white"><a href="/">Logo</a></div>
+    <nav>
+      <ul class="flex gap-4 text-white">
+        <li><a href="/">Home</a></li>
+        <li><a href="/users">Users</a></li>
+        
+        <li><a href="/about">About</a></li>
+        <li class="bg-red-500 px-8 rounded"><a href="/users/create">New</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main class="container flex justify-center">
   <div>
     <h1>User System</h1>
     <p>This project is a learning-focused application built with Laravel, designed to explore the fundamentals of user data handling</p>
   </div>
+  </main>
 </body>
 </html>
